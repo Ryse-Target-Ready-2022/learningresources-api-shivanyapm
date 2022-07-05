200cfa875e2cb3b425f21a793b8b883b7c2bad10
Shivanyapm <shivanyapm22@gmail.com>
2022-07-05 20:17:30 +0530

    Hello World commit

---
+++ src/main/java/com/javaproject/learningresourcesapishivanyapm/LearningresourcesApiShivanyapmApplication.java
@@ -0,0 +1,14 @@
+package com.javaproject.learningresourcesapishivanyapm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiShivanyapmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiShivanyapmApplication.class, args);
+		System.out.println("Hello World :) ");
+	}
+
+}
---
+++ src/test/java/com/javaproject/learningresourcesapishivanyapm/LearningresourcesApiShivanyapmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.learningresourcesapishivanyapm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesApiShivanyapmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
